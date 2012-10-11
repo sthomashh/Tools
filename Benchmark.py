fbbd3e9db68b08ad9d4d85f38b51964cc6e34f0e
root <sthomas@gwdg.de>
2012-10-11 13:17:46 +0200

    in Klassen aufgeteilt

---
+++ Benchmark.py
@@ -0,0 +1,93 @@
+#!/usr/bin/python
+#
+# @author:    Sven Thomas, sthomas@gwdg.de
+
+import re
+import os
+import sys
+import time
+
+
+class Benchmark (object): 
+    debug = 1
+    image_id = ""
+    vm_name = ""
+    vm_amount = 0
+    flavor = 0
+    wait_time = 0
+    ids_started_vms = ""
+    
+    def __init__(self, vm_amount=5, flavor=1, vm_name='TEST', wait_time=5.0): 
+        self.vm_amount = vm_amount 
+        self.flavor = flavor 
+        self.vm_name = vm_name
+        self.wait_time = wait_time
+        
+    def setImageId(self, image_id):
+        self.image_id = image_id.strip()
+            
+    def startVMs(self):
+        self.vm_amount = int(self.vm_amount)
+        self.flavor = str(self.flavor)    
+        self.vm_name = self.vm_name.strip()
+        
+        for i in range(self.vm_amount):
+            identifier = str(i)
+            cmd = '/usr/bin/nova boot --flavor '+self.flavor+' --image '+self.image_id+' '+self.vm_name + "_"+ identifier
+            print cmd
+            if(not self.debug): print os.system(cmd)
+            sys.stdout.flush() 
+                   
+            cmd = '/usr/bin/nova  show '+ self.vm_name + '_' + identifier
+            if(not self.debug): print os.system(cmd)
+            print cmd
+            sys.stdout.flush()
+            time.sleep(self.wait_time);
+            
+
+    def getImageIds(self):
+        pattern = '\s?\|\s?([A-Za-z0-9]+-[A-Za-z0-9]+-[A-Za-z0-9]+-[A-Za-z0-9]+-[A-Za-z0-9]+).+'
+        
+        image_ids = list()
+        s = '''+--------------------------------------+--------------------+--------+------------------------+
+        |                  ID                  |        Name        | Status |        Networks        |
+        +--------------------------------------+--------------------+--------+------------------------+
+        | 6ac61596-0cb7-492e-ae74-ee848b1b429f | UDesktop.64        | ACTIVE | novanetwork=10.10.10.3 |
+        | 98748969-f8f0-4a7f-9b1d-e07e8262fe5c | TEST_1             | ACTIVE | novanetwork=10.10.10.5 |
+        | afbef7b4-a05d-469c-9432-59ecfa540a2f | UbuntuDesktopSven1 | ACTIVE | novanetwork=10.10.10.2 |
+        | b79f29cf-69c3-4b3d-95ba-e676d8a847d2 | TEST_2             | ACTIVE | novanetwork=10.10.10.6 |
+        | c6bb30ce-c0c2-4251-85b5-80f27b9751cc | TEST_0             | ACTIVE | novanetwork=10.10.10.4 |
+        +--------------------------------------+--------------------+--------+------------------------+'''
+        #print pattern
+        #sys.stdout.flush() 
+        s = self.listVMs()
+        for line in s.splitlines():
+           #print pat.findall(line)
+           match = re.search(pattern, line)
+           #print "line:"+line
+           if match:
+               if re.search(self.vm_name+"_\d+", line):
+                   image_ids.append(match.group(1))
+                         
+        return image_ids
+        
+    def listVMs(self):
+        cmd = '/usr/bin/nova list'
+        return os.system(cmd)
+            
+            
+    def setDeleteFile(self, filename):
+        self.image_id = image_id.strip()
+        os.chdir(os.getcwd() + "/")
+        filename = 'delete_'+self.vm_name+'-vmpool.sh'
+        f = open(filename,"w+")
+        content = "#!/bin/bash\n\n"
+        content += "echo 'deleting corrent VMs: \n"
+        matchObj = re.match("(^[A-Za-z0-9\-]).*", f, re.M|re.I)
+        if matchObj: erg += f+"\n"
+        try:
+            for line in f:
+                fileasstring += line
+        finally:
+            f.close()
+            
--- openStack.loadtest.py
+++ openStack.loadtest.py
@@ -13,26 +13,7 @@ import time
 
 debug = 1
 #####################################################################################
-def startVMs(image_id, vm_amount=5, flavor=1, vm_name='TEST', wait_time=5.0):
-    global debug
-    vm_amount = int(vm_amount)
-    flavor = str(flavor)    
-    vm_name = vm_name.strip()
-    
-    for i in range(vm_amount):
-        identifier = str(i)
-        cmd = '/usr/bin/nova boot --flavor '+flavor+' --image '+image_id+' '+vm_name + "_"+ identifier
-        print cmd
-        if(not debug): print os.system(cmd)
-        sys.stdout.flush() 
-               
-        cmd = '/usr/bin/nova  show '+ vm_name + '_' + identifier
-        if(not debug): print os.system(cmd)
-        print cmd
-        sys.stdout.flush()
-        time.sleep(wait_time);
-        
-
+from Benchmark import Benchmark 
 
 message = 'Please give me the openrc file and path (default is /root/openrc): '
 identityfile = raw_input(message+"\n:-> ").strip()
@@ -80,12 +61,28 @@ if vm_name == "": vm_name = "TEST"
 message = 'Please insert a wait-time between the vm startups (default is 5.0 [sec]): '
 wait_time = raw_input(message+"\n:-> ").strip()
 if wait_time == "":
-    wait_time = 5.0
+    wait_time = 0.0
 else:
     wait_time = float(wait_time)
 
-startVMs(image_id, vm_amount, flavor, vm_name, wait_time)
-    
+
+
+B1 = Benchmark()
+B1.debug = 1
+
+B1.vm_amount = vm_amount
+B1.flavor = flavor
+B1.vm_name = vm_name
+B1.wait_time = wait_time
+B1.setImageId(image_id)
+B1.startVMs()
+
+
+image_ids = B1.getImageIds()
+
+for id in image_ids:
+    print "nova delete "+id+"\n"
+
 
 
 
